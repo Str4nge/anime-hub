4342b0b49ef2197217cae35b0bf1fa95c6b0b812
Str4nge <code.amines@gmail.com>
2021-07-01 15:12:47 +0530

    added kitsu API

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const API_BASE_URL = 'https://kitsu.io/api/edge/anime';
+
+export const getApiResult = async queryString => {
+  const response = await fetch(`${API_BASE_URL}?${queryString}`).then(res => {
+    return res.json();
+  });
+  return response;
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { getApiResult } from '../config';
 
 const Home = () => {
-  return <MainPageLayout>This is the Home Page</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+
+  const search = () => {
+    getApiResult(`filter[text]=${input}`).then(result => {
+      setResults(result.data);
+      console.log(result.data);
+    });
+  };
+
+  const keyPress = ev => {
+    if (ev.keyCode === 13) {
+      search();
+    }
+  };
+  const inputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const renderResult = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => {
+            return (
+              <div key={item.id}>
+                {' '}
+                {item.attributes.titles.en_jp} | {item.attributes.titles.ja_jp}
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={inputChange}
+        value={input}
+        onKeyDown={keyPress}
+      />
+      <button type="button" onClick={search}>
+        Search
+      </button>
+      {renderResult()}
+    </MainPageLayout>
+  );
 };
 
 export default Home;
